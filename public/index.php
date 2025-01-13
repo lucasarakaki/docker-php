a8c3962eed1b35c6c7937fb243001fd0a3321063
Lucas Takeshi Arakaki <lucastakeshi@agilitytelecom.com.br>
2025-01-13 09:11:02 -0300

    feat: adicionado uma instância do PDO para testar a conexão com o mariadb

--- public/index.php
+++ public/index.php
@@ -2,3 +2,17 @@
 
 // autoload do composer
 require __DIR__ . '/../vendor/autoload.php';
+
+// Criando instância PDO
+$pdo = null;
+
+try {
+    // instância PDO
+    $pdo = new PDO('mysql:host=mariadb-docker;dbname=php', 'root', 'toor');
+} catch (PDOException $e) {
+    // mensagem de erro ao tentar se conectar ao banco
+    echo $e->getMessage();
+    die;
+}
+
+phpinfo();
